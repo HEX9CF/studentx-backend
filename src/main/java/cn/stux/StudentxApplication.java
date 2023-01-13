18b86f58658a08a3d6710687a4edb623ea6eab73
HEX9CF <hex9cf@163.com>
2023-01-13 18:24:48 +0800

    创建项目

---
+++ src/main/java/cn/stux/StudentxApplication.java
@@ -0,0 +1,13 @@
+package cn.stux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/stux/StudentxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.stux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
